dfdbb330c2646fac49b719d8e8b47570fab377a7
刘文军 <463805737@qq.com>
2021-05-31 22:54:08 +0800

    修改启动类

--- flow-admin/src/main/java/com/dragon/flow/main/FlowAdminApplication.java
+++ flow-admin/src/main/java/com/dragon/flow/main/FlowAdminApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -20,7 +21,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAspectJAutoProxy
 @EnableScheduling
 @EnableTransactionManagement
-@SpringBootApplication(scanBasePackages = {"com.dragon.flow"})
+@SpringBootApplication(scanBasePackages = "com.dragon.flow")
 public class FlowAdminApplication {
     private static final Logger logger = LoggerFactory.getLogger(FlowAdminApplication.class);
 
