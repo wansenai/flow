b627261bd59a889c4e836f917d57a9e2764f5c86
xietongjian <583110978@qq.com>
2021-06-09 14:07:40 +0800

    重新打包前端代码。

---
+++ flow-admin/src/main/resources/static/assets/account.cbd90b5e.js
@@ -0,0 +1,1 @@
+import{av as o}from"./index.f36f9bca.js";var s,t;(t=s||(s={})).ACCOUNT_INFO="/account/getAccountInfo",t.SESSION_TIMEOUT="/user/sessionTimeout";const a=()=>o.get({url:s.ACCOUNT_INFO});export{a};
