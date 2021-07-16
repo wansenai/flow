dac42b88f4813c28f7137a9831144cb5f7f0e1a1
刘文军 <463805737@qq.com>
2021-07-16 16:13:25 +0800

    juel表达式解析

--- flow-api/src/main/java/com/dragon/flow/vo/flowable/model/ModelInfoVo.java
+++ flow-api/src/main/java/com/dragon/flow/vo/flowable/model/ModelInfoVo.java
@@ -15,11 +15,11 @@ import java.io.Serializable;
 @Data
 @ApiModel(value = "ModelInfoVo", description = "查询模型对象返回对象")
 public class ModelInfoVo implements Serializable {
-
     private static final long serialVersionUID = -2434943659168309903L;
     private String modelId;
     private String modelKey;
     private String modelName;
     private String fileName;
     private String modelXml;
+    private String appSn;
 }
--- flow-core/src/main/java/com/dragon/flow/juel/FlowJuel.java
+++ flow-core/src/main/java/com/dragon/flow/juel/FlowJuel.java
@@ -5,10 +5,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @Description: 个性化处理表达式
@@ -22,71 +19,49 @@ public class FlowJuel implements Serializable {
 
     /**
      * 通过传入的参数获取其中的一个人
+     *
      * @param applyers 审批人
      * @return
      */
     public String one(String applyers) {
         String applyer = "";
-        if (StringUtils.isNotBlank(applyers)){
+        if (StringUtils.isNotBlank(applyers)) {
             String[] userCodes = applyers.split(",");
             applyer = userCodes[0];
         }
         return applyer;
     }
+
     /**
      * 通过传入的数据解析有多少个人员
-     * 使用方式${flowJuel.multi(form.startName,form.mgegr,form.test)}
+     * 使用方式${fuel.multi(form.startName,form.mgegr,form.test)}
      *
-     * @param applyer1 传入的数据
+     * @param applyers 传入的数据
      * @return
      */
-    public List<String> multi(String applyer1, String applyer2, String applyer3, String applyer4, String applyer5) {
+    public List<String> multi(String... applyers) {
         Set<String> applyerList = new HashSet<>();
-        if (StringUtils.isNotBlank(applyer1)){
-            this.getApplerCodes(applyerList, applyer1);
-        }
-        if (StringUtils.isNotBlank(applyer2)){
-            this.getApplerCodes(applyerList, applyer2);
-        }
-        if (StringUtils.isNotBlank(applyer3)){
-            this.getApplerCodes(applyerList, applyer3);
-        }
-        if (StringUtils.isNotBlank(applyer4)){
-            this.getApplerCodes(applyerList, applyer4);
-        }
-        if (StringUtils.isNotBlank(applyer5)){
-            this.getApplerCodes(applyerList, applyer5);
+        if (applyers != null && applyers.length > 0) {
+            for(String applyer : applyers) {
+                if (StringUtils.isNotBlank(applyer)) {
+                    this.getApplerCodes(applyerList, applyer);
+                }
+            }
         }
         return new ArrayList<>(applyerList);
     }
 
     private void getApplerCodes(Set<String> applyerList, String applyers) {
         Set<String> codes = new HashSet<>();
-        if (StringUtils.isNotBlank(applyers)){
+        if (StringUtils.isNotBlank(applyers)) {
             String[] userCodes = applyers.split(",");
-            for(String userCode : userCodes){
+            for (String userCode : userCodes) {
                 codes.add(userCode);
             }
         }
-        if (CollectionUtils.isNotEmpty(codes)){
+        if (CollectionUtils.isNotEmpty(codes)) {
             applyerList.addAll(codes);
         }
-    }
-
-    public List<String> multi(String applyer1, String applyer2, String applyer3, String applyer4) {
-        return this.multi(applyer1, applyer2, applyer3, applyer4, null);
-    }
-
-    public List<String> multi(String applyer1, String applyer2, String applyer3) {
-        return this.multi(applyer1, applyer2, applyer3, null, null);
-    }
-
-    public List<String> multi(String applyer1, String applyer2) {
-        return this.multi(applyer1, applyer2, null, null, null);
-    }
-
-    public List<String> multi(String applyer1) {
-        return this.multi(applyer1, null, null, null, null);
     }
 
     /**
@@ -98,7 +73,7 @@ public class FlowJuel implements Serializable {
      */
     public boolean exist(String str, String searchStr) {
         boolean flag = false;
-        if (StringUtils.isNotBlank(str) && StringUtils.isNotBlank(searchStr)){
+        if (StringUtils.isNotBlank(str) && StringUtils.isNotBlank(searchStr)) {
             flag = StringUtils.contains(str, searchStr);
         }
         return flag;
