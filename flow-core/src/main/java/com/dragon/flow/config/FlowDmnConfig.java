a6e63cb44e8f9ae0d37b68c1f630ec8aa3e4d6cc
刘文军 <463805737@qq.com>
2021-07-22 08:49:44 +0800

    测试监听

--- flow-core/src/main/java/com/dragon/flow/config/FlowDmnConfig.java
+++ flow-core/src/main/java/com/dragon/flow/config/FlowDmnConfig.java
@@ -3,6 +3,7 @@ package com.dragon.flow.config;
 import org.flowable.dmn.editor.converter.DmnJsonConverter;
 import org.flowable.dmn.spring.SpringDmnEngineConfiguration;
 import org.flowable.dmn.xml.converter.DmnXMLConverter;
+import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,8 @@ public class FlowDmnConfig implements EngineConfigurationConfigurer<SpringDmnEng
 
     @Override
     public void configure(SpringDmnEngineConfiguration configuration) {
-
+        //设置是否升级 false不升级  true升级
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
     }
 
     @Bean
