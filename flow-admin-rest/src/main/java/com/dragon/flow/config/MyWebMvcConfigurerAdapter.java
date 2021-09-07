6ca6dcab333186b9620ba28e2df019c4c3712845
刘文军 <463805737@qq.com>
2021-09-08 00:13:59 +0800

    升级springboot到2.5.4 跨域问题解决

--- flow-admin-rest/src/main/java/com/dragon/flow/config/MyWebMvcConfigurerAdapter.java
+++ flow-admin-rest/src/main/java/com/dragon/flow/config/MyWebMvcConfigurerAdapter.java
@@ -3,11 +3,8 @@ package com.dragon.flow.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.flowable.ui.common.security.CustomPersistentRememberMeServices;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -61,14 +58,14 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
