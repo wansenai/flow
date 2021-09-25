0f8b77e8c71e355fd82013460b602abc21a1c705
bruce.liu <463805737@qq.com>
2021-09-25 09:56:07 +0800

    优化自定义扩展组件

--- flow-api/src/main/java/com/dragon/flow/model/org/Company.java
+++ flow-api/src/main/java/com/dragon/flow/model/org/Company.java
@@ -23,7 +23,10 @@ public class Company extends BaseModel implements Serializable{
     private static final long serialVersionUID = -8526694883060760613L;
     @TableId(type = IdType.ASSIGN_UUID)
     private String id;
-    
+    /**
+     * 公司类型
+     */
+    private String type;
     /**
      * 上级公司id
      */
