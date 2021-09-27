f39d0870826f4db12583a4f7bb78a80c5396a135
xietongjian <583110978@qq.com>
2021-09-27 12:34:45 +0800

    '流程设计器功能BUG修复，前端重新打包'

---
+++ flow-admin/src/main/resources/static/assets/account.76e98f74.js
@@ -0,0 +1,1 @@
+import{aw as o}from"./index.10e3b51a.js";var e,s;(s=e||(e={})).ACCOUNT_INFO="/account/getAccountInfo",s.SESSION_TIMEOUT="/user/sessionTimeout";const t=()=>o.get({url:e.ACCOUNT_INFO});export{t as a};
---
+++ flow-admin/src/main/resources/static/assets/upload.1dc52c5b.js
@@ -0,0 +1,1 @@
+import{u as o,aw as r}from"./index.10e3b51a.js";const{uploadUrl:a=""}=o();function s(o,s){return r.uploadFile({url:a,onUploadProgress:s},o)}export{s as u};
