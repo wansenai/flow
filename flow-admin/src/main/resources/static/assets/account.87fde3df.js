b390bde9c790048612156dc9763004d799b19529
xietongjian <583110978@qq.com>
2021-10-28 14:31:03 +0800

    解决flow-admin-ui前端打包后流程图无法访问的问题。
    (cherry picked from commit 178e1b1f544be450c2abcae88defb0aa1d89c731)

---
+++ flow-admin/src/main/resources/static/assets/account.87fde3df.js
@@ -0,0 +1,1 @@
+import{aw as o}from"./index.8bbed969.js";var e,s;(s=e||(e={})).ACCOUNT_INFO="/account/getAccountInfo",s.SESSION_TIMEOUT="/user/sessionTimeout";const t=()=>o.get({url:e.ACCOUNT_INFO});export{t as a};
---
+++ flow-admin/src/main/resources/static/assets/index.e5d98295.js
@@ -0,0 +1,1 @@
+import{al as r,af as s,w as t}from"./index.8bbed969.js";import{y as e}from"./vendor.880b4c6c.js";const o=t(e({name:"Authority",props:{value:{type:[Number,Array,String],default:""}},setup(t,{slots:e}){const{hasPermission:o}=r();return()=>function(){const{value:r}=t;return r?o(r)?s(e):null:s(e)}()}}));export{o as A};
