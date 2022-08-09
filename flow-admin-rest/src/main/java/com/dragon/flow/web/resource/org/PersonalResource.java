eca9e8f7abd244257829678d174c9e82ec668dad
xtj <xtj@123456A>
2022-08-09 13:16:24 +0800

    fix：增加通过工号查询人员的接口-改成post请求

--- flow-admin-rest/src/main/java/com/dragon/flow/web/resource/org/PersonalResource.java
+++ flow-admin-rest/src/main/java/com/dragon/flow/web/resource/org/PersonalResource.java
@@ -116,7 +116,7 @@ public class PersonalResource extends BaseResource<Personal> {
      * @param codes
      * @return
      */
-    @GetMapping(value = "/getByCodes", produces = "application/json")
+    @PostMapping(value = "/getByCodes", produces = "application/json")
     public ReturnVo<List<Personal>> getByCodes(@RequestBody List<String> codes) {
         ReturnVo<List<Personal>> returnVo = new ReturnVo<>(ReturnCode.FAIL, "查询异常！");
         List<Personal> personals = new ArrayList<>();
