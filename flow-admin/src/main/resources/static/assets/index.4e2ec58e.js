67e96055822aa15290a1b078357bfa1a0f9235cb
xtj <xtj@123456A>
2023-02-03 17:11:44 +0800

    fix：后台管理前端项目打包

---
+++ flow-admin/src/main/resources/static/assets/index.4e2ec58e.js
@@ -0,0 +1,1 @@
+import{R as a,C as r}from"./Col.55ed0272.js";import{V as o}from"./index.ef013507.js";var l=o(a),m=o(r);export{m as C,l as R};
---
+++ flow-admin/src/main/resources/static/assets/tree.1ef2b57c.js
@@ -0,0 +1,1 @@
+import{dP as e}from"./index.e61ad282.js";const s=t=>e.get({url:"/select/getDemoOptions",params:t}),i=t=>e.get({url:"/tree/getDemoOptions",params:t});export{s as o,i as t};
