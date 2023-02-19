f5844480414263e2c3468eef5eb7c72b75c13e89
min.wang <wangmin082@163.com>
2023-02-19 16:18:12 +0800

    feat:新增forminfo 和formDataInfo 相关类

---
+++ flow-form/flow-form-core/src/main/java/com/dragon/flow/mapper/IFormInfoMapper.java
@@ -0,0 +1,16 @@
+package com.dragon.flow.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dragon.flow.form.model.FormInfo;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @program: flow
+ * @description: 自定义表单mapper
+ * @author: min.wang
+ * @create: 2023-02-19 10:00
+ **/
+@Repository
+public interface IFormInfoMapper extends BaseMapper<FormInfo> {
+
+}
