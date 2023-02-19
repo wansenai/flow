33aa4b2eb4d0985d2e4f1a734e63129ed2d517ca
bruce.liu <463805737@qq.com>
2023-02-19 12:13:13 +0800

    添加自定义表单模块

---
+++ flow-form/flow-form-api/src/main/java/com/dragon/flow/form/model/FormDataInfo.java
@@ -0,0 +1,40 @@
+package com.dragon.flow.form.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.dragon.tools.common.BaseModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ *   @author bruce.liu
+ *  @Comment:系统信息
+ *  @Create  Date 2023年02月19日
+ *
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName(value = "tbl_form_datainfo")
+public class FormDataInfo extends BaseModel implements Serializable {
+    @TableId(type = IdType.ASSIGN_UUID)
+    private String id;
+    /**
+     * 流程定义key
+     */
+    private String modelKey;
+    /**
+     * 业务单据id
+     */
+    private String businessKey;
+    /**
+     * 流程实例id
+     */
+    private String processInstanceId;
+    /**
+     * 表单数据
+     */
+    private String formData;
+}
