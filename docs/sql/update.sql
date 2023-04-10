1620e57ae9acc69e6a2afb52cdf3b942a0056917
XIETONGJIAN <583110978@qq.com>
2023-04-10 18:30:33 +0800

    fix:修改人员表

--- docs/sql/update.sql
+++ docs/sql/update.sql
@@ -3,3 +3,7 @@ ALTER TABLE `tbl_org_personal`
     ADD COLUMN `third_party` VARCHAR (32) NULL COMMENT '三方平台（DingTalk:钉钉，FeiShu:飞书；WeiXin:微信）' AFTER `fax`
     , ADD COLUMN `third_user_id` VARCHAR (64) NULL COMMENT '三方平台用户ID' AFTER `third_party`;
 
+
+ALTER TABLE `tbl_org_personal`
+    MODIFY COLUMN `third_user_id` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '三方平台的userid' AFTER `dept_id`,
+    ADD COLUMN `third_union_id` VARCHAR(100) NULL COMMENT '三方平台的unionId' AFTER `third_user_id`
