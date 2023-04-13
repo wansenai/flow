10204b7ea6f5ccd4e22d8e3fddca2fabab2d3a9c
XIETONGJIAN <583110978@qq.com>
2023-04-13 14:45:24 +0800

    fix:返回审批记录时将审批人的头像赋上值。

--- flow-core/src/main/java/com/dragon/flow/service/flowable/impl/CommentInfoServiceImpl.java
+++ flow-core/src/main/java/com/dragon/flow/service/flowable/impl/CommentInfoServiceImpl.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dragon.flow.enm.flowable.runtime.CommentTypeEnum;
 import com.dragon.flow.mapper.flowable.ICommentInfoMapper;
 import com.dragon.flow.model.flowable.CommentInfo;
+import com.dragon.flow.model.org.Personal;
 import com.dragon.flow.service.flowable.ICommentInfoService;
+import com.dragon.flow.service.org.IPersonalService;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -27,6 +32,8 @@ public class CommentInfoServiceImpl extends ServiceImpl<ICommentInfoMapper, Comm
 
     @Autowired
     private ICommentInfoMapper commentInfoMapper;
+    @Autowired
+    private IPersonalService personalService;
 
     @Override
     public void saveComment(CommentInfo commentInfo) {
@@ -40,8 +47,21 @@ public class CommentInfoServiceImpl extends ServiceImpl<ICommentInfoMapper, Comm
         List<CommentInfo> commentInfoList = new ArrayList<>();
 
         if (CollectionUtils.isNotEmpty(commentInfos)) {
+            // 渲染数据时把头像赋值后返回
+            List<String> personalCodes = commentInfos.stream().map(CommentInfo::getPersonalCode).distinct().collect(Collectors.toList());
+            List<Personal> personals = personalService.getPersonalsByCodes(personalCodes);
+            Map<String, Personal> personalMap = personals.stream().collect(Collectors.toMap(Personal::getCode, Function.identity()));
+
             for (CommentInfo commentInfo : commentInfos) {
                 commentInfo.setTypeName(CommentTypeEnum.getEnumMsgByType(commentInfo.getType()));
+                if(personalMap.containsKey(commentInfo.getPersonalCode())){
+                    Personal personal = personalMap.get(commentInfo.getPersonalCode());
+                    commentInfo.setPersonalHeadImg(personal.getHeadImg());
+                    if(StringUtils.isBlank(commentInfo.getPersonalName())){
+                        commentInfo.setPersonalName(personal.getName());
+                    }
+                }
+
                 commentInfoList.add(commentInfo);
                 if (CommentTypeEnum.LCZZ.name().equals(commentInfo.getType())
                         || CommentTypeEnum.SPJS.name().equals(commentInfo.getType())) {
